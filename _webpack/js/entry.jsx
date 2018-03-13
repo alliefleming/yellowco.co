cf732c0a80415592564629a18d9719fa05344826
Brandon Arbini <b@arbini.com>
2018-03-13 14:34:25 -0700

    Making current focus editable. Better layout on category and series pages.
    Former-commit-id: 17c0b26fe0c677476af1e8fb768463b86ac76fa1

--- _webpack/js/entry.jsx
+++ _webpack/js/entry.jsx
@@ -1,23 +1,30 @@
-import React from 'react';
-import { render } from 'react-dom';
-import $ from 'jquery';
-import 'bootstrap';
-import 'bigslide';
-import 'fotorama/fotorama';
-import Instagram from './components/instagram';
+import React from "react";
+import { render } from "react-dom";
+import $ from "jquery";
+import "bootstrap";
+import "bigslide";
+import "fotorama/fotorama";
+import Instagram from "./components/instagram";
+import Masonry from "masonry-layout";
 
-$('.nav-site .navbar-toggler').bigSlide({
-  menu: '.nav-mobile',
-  push: '.site-nav-push',
-  side: 'right',
+$(".nav-site .navbar-toggler").bigSlide({
+  menu: ".nav-mobile",
+  push: ".site-nav-push",
+  side: "right",
   easyClose: true
 });
 
-$('[data-href]').on('click', function(e) {
-  window.location = $(e.currentTarget).data('href');
+$("[data-href]").on("click", function(e) {
+  window.location = $(e.currentTarget).data("href");
 });
 
 $('[data-toggle="tooltip"]').tooltip();
 
-if (document.getElementById('instagram'))
-  render(<Instagram />, document.getElementById('instagram'));
+if (document.getElementById("instagram"))
+  render(<Instagram />, document.getElementById("instagram"));
+
+new Masonry(".masonry-cards", {
+  horizontalOrder: true,
+  itemSelector: ".card",
+  gutter: ".card-gutter"
+});
